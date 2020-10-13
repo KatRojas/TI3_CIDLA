8c3072e975679cacb267c2a58694c56ed395aa1f
Juan <jzuniga2016@alu.uct.cl>
2020-10-13 13:03:23 -0300

    Integracion Django con MongoDB
    Utilizar Djongo

--- TI3/settings.py
+++ TI3/settings.py
@@ -73,10 +73,17 @@ WSGI_APPLICATION = 'TI3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'TI3_db',
     }
 }
 
