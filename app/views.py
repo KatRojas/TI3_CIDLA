45fe3cfe41889bd85a17abc6d87f782834af29ac
Juan <jzuniga2016@alu.uct.cl>
2020-11-24 18:48:55 -0300

    Recoger Datos desde la DB

--- app/views.py
+++ app/views.py
@@ -17,55 +17,48 @@ from .models import Data
 
 # Create your views here.
 def info(request):
-    #inf = Data.objects.all()
-    #a = []
-    #for field in inf:
-    #    a.append([field.nombre,field.apellido])
-    total = 0
-    cc1 = 0
-    cc2 = 0
-    posts=Posts.objects.all() #en la variable posts guardamos todos los datos que saquemos de la estructura
-    stringval=""
-    for post in posts: #creamos un ciclo para recorrer los datos
-        stringval += "First Name : " + post.user_details['first_name'] + " Last name : " + post.user_details[
-            'last_name'] + " Post Title " + post.post_title + " Comment " + post.comment[0]+"<br>"
-        total+=1
-        nombre = post.user_details['first_name'].lower() #extraemos el nombre y aplicamos lower para colocar el texto en minuscula
-        if nombre == "bryan":
-            cc1= cc1+1
-        elif nombre  == "diego":
-            cc2=cc2+1
-    names = ['Bryan','Diego']
-    values = [cc1,cc2]
-
-    plt.figure(figsize=(9, 3)) #creamos la figura
-    plt.suptitle('Graficos')
-    #subplot nos permite crear distintos graficos para mostrar, subplot(Un entero de 3 dígitos o tres enteros independientes que describen la posición del subgráfico)
-    plt.subplot(131)
-    plt.bar(names, values)
-    plt.subplot(132)
-    plt.scatter(names, values)
-    plt.subplot(133)
-    plt.title('Graficos de Torta')
-    explode = [0,0]  # Destacar algunos
-    plt.pie(values, labels=names, explode=explode,
-        autopct='%1.1f%%')
-    # autopct: El porcentaje correspondiente a cada ítem se puede indicar mediante el argumento 
-    #
+    data = ''
+    dd = ''
+    dato_sesiones = []
+    SiNo = True
+    msj = "Complete todos lo datos Solicitados"
     
-    fig = plt.gcf() #obtenemos la figura actual
-    #convert graph into dtring buffer and then we convert 64 bit code into image
-    buf = io.BytesIO() #estructura binaria
-    fig.savefig(buf,format='png') #guardamos la figura con el formato y estructura
-    buf.seek(0)
-    #establece la posición actual del archivo en el desplazamiento. El argumento whence es opcional y el valor predeterminado es 0, lo que significa posicionamiento absoluto del archivo
-    #otros valores son 1 lo que significa buscar en relación con la posición actual y 2 significa buscar en relación con el final del archivo.
-    string = base64.b64encode(buf.read()) #Codificamos el objeto de tipo bytes mediante Base64 y se devuelven los bytes codificados.
-    uri =  urllib.parse.quote(string) 
-    #parse.quote nos permite citar localmente y hacer versiones seguras de las cadenas que contienen
-    #caracteres especiales dentro de los argumentos de consulta que podrían causar problemas en el análisis de la URL en el lado del servidor
-    return render(request,'info.html',{"stringval": stringval, "cc1": cc1,"cc2":cc2,"data":uri,"total": total}) 
-    #parametros  del render (1- request, 2- nombre plantilla, 3- contexto (variables))
+    if request.method == 'POST':
+
+        search = request.POST['search']
+        inicio = request.POST['inicio']
+        fin = request.POST['fin']
+        
+        if search == '' or inicio == '' or fin == '':
+            SiNo = False
+        else:
+            inicio1 = inicio.split("-")
+            inicio2 = inicio1[2]+"/"+inicio1[1]+"/"+inicio1[0]
+            fin1 = fin.split("-")
+            fin2 = fin1[2]+"/"+fin1[1]+"/"+fin1[0]
+
+            inicio3 = datetime.strptime(inicio2,'%d/%m/%Y')
+            fin3 = datetime.strptime(fin2,'%d/%m/%Y')
+
+            client = MongoClient('localhost',27017)
+            db = client.ti3
+            col = db.archivojson
+            cursor = col.find({ 'rut':search })
+            for x in cursor:
+                data = x['nombre'] + ' ' + x['apellidos'] 
+                for y in range(len(x['sesiones_medica'])):
+                    fec = x['sesiones_medica'][y]['fecha']
+                    if datetime.strptime(fec,'%d/%m/%Y') >= inicio3 and datetime.strptime(fec,'%d/%m/%Y') <= fin3:
+                        dato_sesiones.append([x["sesiones_medica"][y]])
+                        ff = x['sesiones_medica'][y]['fecha']
+                        if dd == '':
+                            dd = ff
+                        else:
+                            dd = dd +','+ ff
+    if SiNo == False:
+        return render(request,'info.html',{"msj":msj})
+    else:
+        return render(request,'info.html',{"data":data,"dato_fecha":dd,"sesiones":dato_sesiones}) 
 
 
 @csrf_exempt
